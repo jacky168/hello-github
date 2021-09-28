9fcb0bc20962cfeac9d8ccf1d86f7f3e3a6e246c
jacky168@gmail.com <jacky168@gmail.com>
2021-09-28 11:36:33 +0800

    test message

--- example.py
+++ example.py
@@ -1,9 +1,13 @@
-# %% 
-r = 123_456 + 1_000_000 + 1000
+# %%
+r = 123_456 + 1000_000 + 1000
 print(r)
 
 # %%
+print("Hello, World")
+
+# %%
 f = 1.0e-3
+# str(f)
 print(f)
 
 # %%
@@ -14,12 +18,12 @@ This is the second line.
 
 print(s)
 type(s)
-# %% 
+# %%
 # SyntaxError: EOL while scanning string literal
-s2 = '''
+s2 = """
 This is the first line.\
 This is the second line.
-'''
+"""
 print(s2)
 
 # %%
@@ -32,8 +36,8 @@ name = "Henny"
 # # name[0] = "P"
 # print(name.replace("H", "P"))
 print(name)
-print(name.replace('H', 'P'))
-print(name)
+print(name.replace("H", "P"))
+print(name)  # string is immutable
 
 # %%
 name2 = name.replace("H", "P")
@@ -44,17 +48,18 @@ print("memory address for name2: {}".format(hex(id(name2))))
 # %%
 # slicing
 print(name[1:])
-name3 = "P" + name[1:]  
+name3 = "P" + name[1:]
 print("name3: {}".format(name3))
 print("memory address for name: {}".format(hex(id(name))))
 print("memory address for name3: {}".format(hex(id(name3))))
 
 
 # %%
-names = ['Alice', 'Bob', 'Charlie']
+names = ["Alice", "Bob", "Charlie"]
 combined = ",".join(names)
 print(combined)
 
 # %%
-new_names = combined.split(',')
+new_names = combined.split(",")
 print(new_names)
+# %%
